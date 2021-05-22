d0a2503b201bccf1d3548c6831d1c18eb86e0471
alexlopesbr <criabio@hotmail.com>
2021-05-22 10:03:17 -0300

    products fetch in home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,26 @@ import React from 'react';
 import Head from '../components/Head';
 
 const Home = () => {
+  const [datas, setDatas] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch('https://ranekapi.origamid.dev/json/api/produto')
+      .then((r) => r.json())
+      .then((json) => setDatas(json));
+  }, []);
+
+  if (datas === null) return null;
+
   return (
     <>
       <Head title="Home" description="description Home" />
       <h1>Home</h1>
+      {datas.map((data) => (
+        <div key={data.id}>
+          <img src={data.fotos[0].src} alt={data.fotos[0].titulo} />
+          <p>{data.nome}</p>
+        </div>
+      ))}
     </>
   );
 };
